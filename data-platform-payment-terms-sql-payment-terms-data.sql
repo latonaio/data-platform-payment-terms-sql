eef93cfd74ae09091575174e75a4e6dad2e55072
Yurino Oga <yurino.o@latona.io>
2022-10-22 20:34:41 +0900

    release

--- data-platform-payment-terms-sql-payment-terms-data.sql
+++ data-platform-payment-terms-sql-payment-terms-data.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `data_platform_payment_terms_payment_terms_data`
 (
   `PaymentTerms`                 varchar(4) NOT NULL,
-  `DueDate`                      varchar(2) DEFAULT NULL,
-  `BaseDateCalcFixedDate`        varchar(2) DEFAULT NULL,
-  `BaseDateCalcAddMonth`         varchar(2) DEFAULT NULL,
-  `PaymentDateCalcFixedDate`     varchar(2) DEFAULT NULL,
-  `PaymentDateAddMonth`          varchar(2) DEFAULT NULL,
+  `DueDate`                      int(2) DEFAULT NULL,
+  `BaseDateCalcFixedDate`        int(2) DEFAULT NULL,
+  `BaseDateCalcAddMonth`         int(2) DEFAULT NULL,
+  `PaymentDateCalcFixedDate`     int(2) DEFAULT NULL,
+  `PaymentDateAddMonth`          int(2) DEFAULT NULL,
     PRIMARY KEY (`PaymentTerms`),
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
