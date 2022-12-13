7fd0e027c13aaf37111a059e3c8ea39bc4ede8d6
MiyuYokoyama0109 <miyu.y@latona.io>
2022-12-13 15:55:04 +0900

    release

--- data-platform-payment-terms-sql-payment-terms-data.sql
+++ data-platform-payment-terms-sql-payment-terms-data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `data_platform_payment_terms_payment_terms_data`
 (
   `PaymentTerms`                    varchar(4) NOT NULL,
-  `BaseDate`                        int(2) DEFAULT NULL,
+  `BaseDate`                        int(2) NOT NULL,
   `BaseDateCalcAddMonth`            int(2) DEFAULT NULL,
   `BaseDateCalcFixedDate`           int(2) DEFAULT NULL,
   `PaymentDueDateCalcAddMonth`      int(2) DEFAULT NULL,
