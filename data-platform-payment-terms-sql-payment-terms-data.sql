00069442f9c2200eb2d1539ee02580c1ff8dc8bb
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-25 12:42:58 +0900

    release

--- data-platform-payment-terms-sql-payment-terms-data.sql
+++ data-platform-payment-terms-sql-payment-terms-data.sql
@@ -2,12 +2,15 @@ CREATE TABLE `data_platform_payment_terms_payment_terms_data`
 (
   `PaymentTerms`                    varchar(4) NOT NULL,
   `BaseDate`                        int(2) NOT NULL,
-  `BaseDateCalcAddMonth`            int(2) DEFAULT NULL,
-  `BaseDateCalcFixedDate`           int(2) DEFAULT NULL,
-  `PaymentDueDateCalcAddMonth`      int(2) DEFAULT NULL,
-  `PaymentDueDateCalcFixedDate`     int(2) DEFAULT NULL,
-
-    PRIMARY KEY (`PaymentTerms`, `BaseDate`)
+  `BaseDateCalcAddMonth`            int(2) NOT NULL,
+  `BaseDateCalcFixedDate`           int(2) NOT NULL,
+  `PaymentDueDateCalcAddMonth`      int(2) NOT NULL,
+  `PaymentDueDateCalcFixedDate`     int(2) NOT NULL,
+  `CreationDate`                    date NOT NULL,
+  `LastChangeDate`                  date NOT NULL,
+  `IsMarkedForDeletion`             tinyint(1) DEFAULT NULL,
+  
+  PRIMARY KEY (`PaymentTerms`, `BaseDate`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-payment-terms-sql-payment-terms-text-data.sql
+++ data-platform-payment-terms-sql-payment-terms-text-data.sql
@@ -2,8 +2,16 @@ CREATE TABLE `data_platform_payment_terms_payment_terms_text_data`
 (
   `PaymentTerms`              varchar(4) NOT NULL,
   `Language`                  varchar(2) NOT NULL,
-  `PaymentTermsName`          varchar(100) DEFAULT NULL,
-    PRIMARY KEY (`PaymentTerms`, `Language`),
-    CONSTRAINT `DataPlatformPaymentTermsPaymentTermsTextData_fk` FOREIGN KEY (`PaymentTerms`) REFERENCES `data_platform_payment_terms_payment_terms_data` (`PaymentTerms`)
+  `PaymentTermsName`          varchar(100) NOT NULL,
+  `CreationDate`              date NOT NULL,
+  `LastChangeDate`            date NOT NULL,
+  `IsMarkedForDeletion`       tinyint(1) DEFAULT NULL,
+
+    
+  PRIMARY KEY (`PaymentTerms`, `Language`),
+  
+  CONSTRAINT `DataPlatformPaymentTermsTextData_fk` FOREIGN KEY (`PaymentTerms`) REFERENCES `data_platform_payment_terms_payment_terms_data` (`PaymentTerms`),
+  CONSTRAINT `DataPlatformPaymentTermsTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+    
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
